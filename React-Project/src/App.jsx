d8f494cfc0ccc913cce75ef8030b673e560f8181
Dickson Wachira <programmingwithdickson@gmail.com>
2025-01-19 23:11:25 +0300

    Use app as the parent component of propTypes

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,12 +1,13 @@
-import Card from './Card.jsx'
+import Student from './Student.jsx'
 function App() {
   
     return (
       <>
-       <Card/>
-      <Card/>
-      <Card/>
-      </>
+          <Student name="Patrick" age={30} isStudent={true }/>
+          <Student name="Shright" age={64} isStudent={false }/>
+          <Student name="Tommy" age={21} isStudent={true }/>
+          <Student/>
+       </>
     );
 }
 
