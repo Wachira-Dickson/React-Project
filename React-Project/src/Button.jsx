59fac3af873c292556b99eb69ea27fcb85674d47
Dickson Wachira <programmingwithdickson@gmail.com>
2025-01-22 20:08:12 +0300

    Use the onClick event on a button

---
+++ React-Project/src/Button.jsx
@@ -0,0 +1,8 @@
+
+function Button() {
+
+    const handleClick = (e) => e.target.textContent = "Ouch!";
+    
+    return( <button onClick = {(e) => handleClick(e)}>Click Me</button> );
+}
+ export default Button
