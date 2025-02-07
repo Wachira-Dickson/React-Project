25d32dc4e8a9f02dd403425bf608913a80b905fc
Dickson Wachira <programmingwithdickson@gmail.com>
2025-02-07 17:55:51 +0300

    Use useEffect Hook when the value mount changes

--- React-Project/src/Effect.jsx
+++ React-Project/src/Effect.jsx
@@ -1,49 +1,35 @@
-// useEffect() = React Hook that tells React DO SOME CODE WHEN (pick one):
-//              This component re-renders 
-//              This component mounts
-//              The state of a value
 
-// useEffect(function, [dependencies])
-
-// 1. useEffect(() => {})            //Runs after every re-render
-// 2. useEffect(() => {}, [])        //Runs only on mount
-// 3. useEffect(() => {}, [value])   //Runs on mount + when value changes
-
-// USES
-// #1 Event Listeners
-// #2 DOM manipulation
-// #3 Subscriptions (real-time updates)
-// #4 Fetching DATA from an API
-// #5 Clean up when a component unmounts
 
 import React, {useState, useEffect} from "react"
 function Effect(){
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("green");
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
 
     useEffect(() => {
-        document.title = `Count: ${count}, ${color}`;
-    }, [count, color]);
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED");
 
-    function addCount(){
-        setCount(c => c + 1);
-    }
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    }, []);
 
-    function subtractCount(){
-        setCount(c => c - 1);
-    }
+    useEffect(() => {
+        document.title = `Size: ${width} * ${height}` 
+    }, [width, height]);
 
-    function changeColor(){
-        setColor(c => c  === "green" ? "red" : "green")
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
 
     return(
         <>
-        <p style={{color: color}}>Count:{count}</p>
-        <button onClick={addCount}>Add</button>
-        <button onClick={subtractCount}>Subtract</button><br/>
-        <button onClick={changeColor}>Change Color</button>
+        <p>Window Width: {width}px</p>
+        <p>Window Height: {height}px</p>
         </>
     );
 
